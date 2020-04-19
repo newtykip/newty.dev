f605149bf28bcea18501dc5f1e6b1c7c0dc74013
Newt <itsnewtt@outlook.com>
2020-04-19 15:00:15 +0100

    Update pfp, scrobbling, and add font awesome

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -14,49 +14,49 @@ path.fillColor = "red";
 var hue = Math.floor(Math.random() * 360);
 
 function createPath() {
-    // start point
-    path.add(view.bounds.bottomLeft);
-    // evenly distribute n points
-    for (var i = 1; i < points; i++) {
-        path.add(new Point(width / points * i, 0));
-    }
-    // end point
-    path.add(view.bounds.bottomRight);
-    // smooth path
-    path.smooth();
+  // start point
+  path.add(view.bounds.bottomLeft);
+  // evenly distribute n points
+  for (var i = 1; i < points; i++) {
+    path.add(new Point(width / points * i, 0));
+  }
+  // end point
+  path.add(view.bounds.bottomRight);
+  // smooth path
+  path.smooth();
 }
 
 function onFrame(e) {
-    // shift points to cursor
-    maxHeight += (center.y - curPos.y - maxHeight) / 10;
-    cycleHue();
-    // magic numbers
-    for (var i = 1; i < points; i++) {
-		var sinSeed = e.count + (i + i % 10) * 100;
-		var sinHeight = Math.cos(sinSeed / 200) * maxHeight;
-		var yPos = Math.sin(sinSeed / 100) * sinHeight + height;
-        path.segments[i].point.y = yPos;
-    }
+  // shift points to cursor
+  maxHeight += (center.y - curPos.y - maxHeight) / 10;
+  cycleHue();
+  // magic numbers
+  for (var i = 1; i < points; i++) {
+    var sinSeed = e.count + (i + i % 10) * 100;
+    var sinHeight = Math.cos(sinSeed / 200) * maxHeight;
+    var yPos = Math.sin(sinSeed / 100) * sinHeight + height;
+    path.segments[i].point.y = yPos;
+  }
 }
 
 function onMouseMove(e) {
-	curPos = e.point;
-	cycleHue();
+  curPos = e.point;
+  cycleHue();
 }
 
 function onResize() {
-    // redefine bounds
-    center = view.center;
-	width = view.size.width;
-    height = view.size.height;
-    // reset path segments
-    path.segments = [];
-    createPath();
+  // redefine bounds
+  center = view.center;
+  width = view.size.width;
+  height = view.size.height;
+  // reset path segments
+  path.segments = [];
+  createPath();
 }
 
 function cycleHue() {
-    hue++;
-    path.fillColor.hue = hue;
+  hue++;
+  path.fillColor.hue = hue;
 }
 
 createPath();
