3af7e9a6e38e1e5daa1caa3376e1e85846bf004d
newt <newtykins@outlook.com>
2020-12-27 17:56:19 +0000

    Add osu! to the website and song scrobbling

--- assets/js/osu.js
+++ assets/js/osu.js
@@ -1,2 +1,4 @@
-// coming soon...
-// will display osu rank on website!
+fetch(new URL('https://me.newtt.pw/api/osu'))
+    .then(res => res.json())
+    .then(res => { document.getElementById('rank').innerHTML = `(#${res.rank})`; })
+    .catch(err => console.error('There was an issue fetching data about newt\'s osu profile. Sorry!', err));
