9f32d88bcac92b6dbfacb54b8d8d6af807a4b6ac
newt <newtykins@outlook.com>
2020-12-27 19:02:08 +0000

    Fix api overlap

--- assets/js/osu.js
+++ assets/js/osu.js
@@ -3,5 +3,5 @@ const formatNumber = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 // Fetch information about my osu profile
 fetch(new URL('https://me.newtt.pw/api/osu'))
     .then(res => res.json())
-    .then(res => { document.getElementById('rank').innerHTML = `(#${formatNumber(res.ranks.global)})`; })
+    .then(res => { document.getElementById('rank').innerHTML = `(#${formatNumber(res.globalRank)})`; })
     .catch(err => console.error('There was an issue fetching data about newt\'s osu profile. Sorry!', err));
