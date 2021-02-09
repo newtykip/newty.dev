794e940007c01def304749a0ec36209ba2bdfea1
newt <newtykins@outlook.com>
2021-02-09 12:58:44 +0000

    remove osu flag

--- assets/js/song.js
+++ assets/js/song.js
@@ -1,22 +1,21 @@
+// Fetch the element
+const el = document.getElementById('song');
+
 // Function to update the song
 const updateSong = () => {
-    fetch(new URL('https://about.newtt.me/api/scrobbling'))
+    fetch(new URL('http://localhost:3000/api/scrobbling'))
         .then(res => res.json())
         .then(res => {
-            // Fetch the element
-            const el = document.getElementById('song');
-
             if (!res.hasOwnProperty('message')) {
                 // Update the content of the element
-                const artist = res.type === 'spotify' ? res.artists[0].name.toLowerCase() : res.artist;
+                const artist = res.type === 'spotify' ? res.artists[0].name.toLowerCase() : res.artist.toLowerCase();
                 const song = res.name.toLowerCase();
-                const status = res.osu ? ` currently playing:` : ` currently listening to:`
-            
-                switch (res.type) {
-                    case 'spotfy': el.innerHTML = `${status} <a href="${res.url}">${artist} - ${song}</a>`;
-                    case 'soundcloud': el.innerHTML = `${status} <a href="${res.url}"${artist} - ${song}</a>`;
-                    case 'lastfm': el.innerHTML = `${status} ${artist} - ${song}`;
-                }
+                let msg = ' currently listening to: ';
+
+                if (res.type === 'spotify' || res.type === 'soundcloud') msg += `<a href="${res.url}">${artist} - ${song}</a>`;
+                else msg += `${artist} - ${song}`;
+
+                el.innerHTML = msg;
                 
                 // Ensure that the song is visible
                 if (!el.classList.contains('visible')) {
@@ -33,7 +32,13 @@ const updateSong = () => {
                 console.log('Song updated! Currently listening to nothing!');
             }
         })
-        .catch(err => console.error('There was an issue fetching data about newt\'s current song. Sorry!', err));
+        .catch(err => {
+            console.error('There was an issue fetching data about newt\'s current song. Sorry!', err);
+            // Ensure that the song is not visible
+            if (el.classList.contains('visible')) {
+                el.classList.remove('visible');
+            }
+        });
 };
 
 // Check for a new song every 5 seconds
