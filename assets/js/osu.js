872d8f868d785531461b761d6503442f1294a4ce
newt <newtykins@outlook.com>
2021-02-09 11:35:56 +0000

    Add osu! checks to API

--- assets/js/osu.js
+++ assets/js/osu.js
@@ -1,7 +1,7 @@
 const formatNumber = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 
 // Fetch information about my osu profile
-fetch(new URL('https://about.newtt.me/osu'))
+fetch(new URL('https://about.newtt.me/api/osu'))
     .then(res => res.json())
     .then(res => { document.getElementById('rank').innerHTML = `(#${formatNumber(res.globalRank)})`; })
     .catch(err => console.error('There was an issue fetching data about newt\'s osu profile. Sorry!', err));
