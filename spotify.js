d69614b96d8b9227237e18b01c2413acb6e39e8d
newt! <newtykins@outlook.com>
2021-08-10 22:37:33 +0100

    Fix animation

--- spotify.js
+++ spotify.js
@@ -6,11 +6,11 @@ const updateSong = () => {
     fetch(new URL('https://about.newtt.me/api/np'))
         .then(res => res.json())
         .then(res => {
-            if (res.playing) {
+            if (res.isPlaying) {
                 // Update the content of the element
 	            const artist = res.artists[0].name.toLowerCase();
 	            const song = res.name.toLowerCase();
-                el.innerHTML = `newt ${res.isPlaying ? 'is currently' : 'was most recently'} listening to: <a href="${res.url}">${artist} - ${song}</a>`;
+                el.innerHTML = `listening to: <a href="${res.url}">${artist} - ${song}</a>`;
                 
                 // Ensure that the song is visible
                 if (!el.classList.contains('visible')) {
