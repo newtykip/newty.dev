bbb6f246ba707f0a3db1a586536de95bfa127bd1
newt! <newtykins@outlook.com>
2021-08-12 19:16:50 +0100

    Host dependencies locally

--- anim.js
+++ anim.js
@@ -1,9 +1,8 @@
+// Background Animation
 const body = document.body;
 const html = document.documentElement;
 const height = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
 const width = Math.max(body.scrollWidth, body.offsetWidth, html.clientWidth, html.scrollWidth, html.offsetWidth);
-
-console.log(height, width)
 
 VANTA.NET({
     el: 'html',
