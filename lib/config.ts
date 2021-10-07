7345ae4c601d73f4fe36684349dff0c9bb401ce1
newt! <newtykins@outlook.com>
2021-10-07 18:04:57 +0100

    Port the twitch endpoint

--- lib/config.ts
+++ lib/config.ts
@@ -9,11 +9,22 @@ export default {
             apiKey: process.env.OSU_API_KEY,
             newtId: '16009610',
         },
+        twitch: {
+            clientId: process.env.TWITCH_CLIENT_ID,
+            clientSecret: process.env.TWITCH_CLIENT_SECRET,
+            newtId: '428663118',
+        },
     },
     endpoints: {
         osu: {
             getUser: 'https://osu.ppy.sh/api/get_user',
             getAvatar: 'https://a.ppy.sh',
+        },
+        twitch: {
+            getAccessToken: 'https://id.twitch.tv/oauth2/token',
+            getStream: 'https://api.twitch.tv/helix/streams',
+            getUser: 'https://api.twitch.tv/helix/users',
+            getTags: 'https://api.twitch.tv/helix/tags/streams',
         },
     },
 };
