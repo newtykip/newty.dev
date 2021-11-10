febfae09a51d36b37d3338ed7dcb062555fe888b
newt! <newtykins@outlook.com>
2021-11-10 19:40:01 +0000

    Basic website rewrite in React

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,82 @@
-import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import * as THREE from 'three';
+import NET from 'vanta/dist/vanta.net.min.js';
+import Typewriter, { TypewriterClass } from 'typewriter-effect';
+import Helmet from 'react-helmet';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faTwitch } from '@fortawesome/free-brands-svg-icons';
 import './App.css';
+import type { SizeProp } from '@fortawesome/fontawesome-svg-core';
 
 interface AppProps {}
 
 function App({}: AppProps) {
-  // Create the count state.
-  const [count, setCount] = useState(0);
-  // Create the counter (+1 every second).
-  useEffect(() => {
-    const timer = setTimeout(() => setCount(count + 1), 1000);
-    return () => clearTimeout(timer);
-  }, [count, setCount]);
-  // Return the App component.
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <p>
-          Page has been open for <code>{count}</code> seconds.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
-    </div>
-  );
+    const [vantaEffect, setVantaEffect]: any = useState(0);
+    const iconSize: SizeProp = '2x';
+
+    useEffect(() => {
+        // Handle the VANTA animation
+        if (!vantaEffect) {
+            const { innerHeight: height, innerWidth: width } = window;
+
+            setVantaEffect(
+                NET({
+                    el: '.anim',
+                    mouseControls: true,
+                    touchControls: false,
+                    gyroControls: false,
+                    minHeight: height,
+                    minWidth: width,
+                    backgroundColor: 0x0f0f10,
+                    color: 0xffffff,
+                    showDots: false,
+                    points: width >= 1920 ? 3 : Math.floor(width / 100),
+                    THREE,
+                }),
+            );
+        }
+
+        return () => {
+            if (vantaEffect) vantaEffect.destroy();
+        };
+    }, [vantaEffect]);
+
+    const handleTypewriter = (typewriter: TypewriterClass) => {
+        typewriter.typeString('newtykins').start();
+    };
+
+    return (
+        <main>
+            <Helmet>
+                <title>newt!!</title>
+            </Helmet>
+            <span className="anim"></span>
+            <span className="text-silver text-8xl font-extrabold name">
+                <Typewriter onInit={handleTypewriter} />
+            </span>
+            <span className="text-gray-400 text-5xl font-bold py-56 leading-relaxed intro">
+                Student programmer providing simplistic solutions to modern
+                problems.
+            </span>
+
+            <footer className="text-gray-400">
+                <ul className="inline space-x-8">
+                    <a
+                        href="https://github.com/newtykins"
+                        className="hover:text-silver"
+                    >
+                        <FontAwesomeIcon icon={faGithub} size={iconSize} />
+                    </a>
+                    <a
+                        href="https://twitch.tv/newtykin"
+                        className="hover:text-silver"
+                    >
+                        <FontAwesomeIcon icon={faTwitch} size={iconSize} />
+                    </a>
+                </ul>
+            </footer>
+        </main>
+    );
 }
 
 export default App;
