070b92793d28af8cc72278efd7207fdb81daac12
newt! <newtykins@outlook.com>
2021-11-17 21:38:14 +0000

    Add social component

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,13 @@ import * as THREE from 'three';
 import NET from 'vanta/dist/vanta.net.min.js';
 import Typewriter, { TypewriterClass } from 'typewriter-effect';
 import Helmet from 'react-helmet';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub, faTwitch } from '@fortawesome/free-brands-svg-icons';
+import Social from './components/Social';
 import './App.css';
-import type { SizeProp } from '@fortawesome/fontawesome-svg-core';
 
 interface AppProps {}
 
 function App({}: AppProps) {
     const [vantaEffect, setVantaEffect]: any = useState(0);
-    const iconSize: SizeProp = '2x';
 
     useEffect(() => {
         // Handle the VANTA animation
@@ -50,30 +47,20 @@ function App({}: AppProps) {
             <Helmet>
                 <title>newt!!</title>
             </Helmet>
+
             <span className="anim"></span>
+
             <span className="text-silver text-8xl font-extrabold name">
                 <Typewriter onInit={handleTypewriter} />
             </span>
+
             <span className="text-gray-400 text-5xl font-bold py-56 leading-relaxed intro">
                 Student programmer providing simplistic solutions to modern
                 problems.
             </span>
 
             <footer className="text-gray-400">
-                <ul className="inline space-x-8">
-                    <a
-                        href="https://github.com/newtykins"
-                        className="hover:text-silver"
-                    >
-                        <FontAwesomeIcon icon={faGithub} size={iconSize} />
-                    </a>
-                    <a
-                        href="https://twitch.tv/newtykin"
-                        className="hover:text-silver"
-                    >
-                        <FontAwesomeIcon icon={faTwitch} size={iconSize} />
-                    </a>
-                </ul>
+                <Social iconSize={'2x'} />
             </footer>
         </main>
     );
