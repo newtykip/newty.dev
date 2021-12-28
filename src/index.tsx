91e43e4bf78626831871ee91f93b1c9ffc940f71
newtykins <newtykins@outlook.com>
2021-12-28 03:54:50 +0000

    Add ogp and analytics

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactGA from 'react-ga';
 import App from './App';
 import './index.css';
+
+ReactGA.initialize('G-X2HWB0MHZ2');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(
     <React.StrictMode>
