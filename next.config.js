39f412f0a1b7ceb6135b45084ab437ddedfa1097
newtykins <newtykins@outlook.com>
2022-03-19 18:51:29 +0000

    fix: didn't configure eslint properly

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false,
-    ignoreDuringBuilds: true,
+    eslint: {
+        ignoreDuringBuilds: true
+    },
     images: {
         domains: ['github.com']
     }
